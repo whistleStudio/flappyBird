9cf91fcb1b9f38849dc19e3f01107220d73ea9b0
whistle <435424527@qq.com>
2025-07-30 15:14:12 +0800

    1st commit

---
+++ assets/scripts/bgControl.ts
@@ -0,0 +1,9 @@
+import { _decorator, Component, Node } from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass("bgControl")
+export class bgControl extends Component {
+  start() {}
+
+  update(deltaTime: number) {}
+}
